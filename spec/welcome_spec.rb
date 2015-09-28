844db7f2b1b1e330adc15b740cfb490ee978e163
AnnJohn <anndjohn@gmail.com>
2015-09-28 11:06:20 -0400

    Fixed typo in spec file

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,5 +1,5 @@
 describe "lib/welcome.rb" do
-  it 'prints "Welcome to Tic Tac Toe"' do
+  it 'prints "Welcome to Tic Tac Toe!"' do
     expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure lib/welcome.rb has code that can output 'Welcome to Tic Tac Toe!' exactly."
     
     load './lib/welcome.rb'
