5cf0fe31fa6879a19b48cdce51cace602aafa89e
Rihad <rihadv021@gmail.com>
2018-05-17 12:11:09 +0000

    Done.

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,5 +1,5 @@
 # Edit this file to output "Welcome to Tic Tac Toe!"
 # You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
-put "Welcome to Tic Tac Toe!"
+puts "Welcome to Tic Tac Toe!"
 lib/welcome.rb
